0acecd4ee11ecf9137faa2ef2bbc8a1a73c5a21b
Kuba <jakub.mikrut@motteq.com>
2014-02-27 23:43:10 +0100

    cross domain v1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  
+  #todo
   # protect_from_forgery with: :exception
+  after_filter :cors_set_access_control_headers
+  
+  private
+
+    def cors_set_access_control_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'X-AUTH-TOKEN, X-API-VERSION, X-Requested-With, Content-Type, Accept, Origin'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Max-Age'] = "1728000"
+    end
 end
